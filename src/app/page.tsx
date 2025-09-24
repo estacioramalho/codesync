d35b355076fa3bfdaace61e07a8960d814bbb101
Estácio Ramalho <estacioramalhodev@gmail.com>
2025-09-24 13:47:10 -0300

    feat: integrate Clerk for authentication and add Convex for backend functionality
    - Updated package.json to include @clerk/nextjs and convex dependencies.
    - Wrapped the RootLayout component with ClerkProvider for authentication context.
    - Modified the Home component to prepare for user authentication with Clerk.
    - Added Convex functions directory with README and generated files for API and data model.
    - Defined a schema for users in Convex with necessary fields and indexes.
    - Implemented middleware for Clerk to protect routes and API endpoints.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import {
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Button>Sign In</Button>
-    </div>
-  );
+  return <div className="m-10"> Home Page</div>;
 }
